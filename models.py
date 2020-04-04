a508333c86e549efd631f19b5b38d79b9c38f425
jlcgg <janete.carramona@gmail.com>
2020-04-04 22:54:36 +0100

    merge conflit

--- models.py
+++ models.py
@@ -10,3 +10,41 @@ class Lamp:
 
     def set_off(self):
         self.state = False
+
+class ColorLamp(Lamp):
+    def __init__(self):
+        self.color = 'black'
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+class LampArray(ColorLamp):
+    def __init__(self):
+        self.lamp_list = []
+
+    def add_lamp(self, lamp):
+        self.lamp_list.append(lamp)
+        return lamp
+
+    def set_on(self):
+        for lamp in self.lamp_list:
+            lamp.set_on()
+
+    def set_off(self):
+        for lamp in self.lamp_list:
+            lamp.set_off()
+
+    def check_on(self):
+        for lamp in self.lamp_list:
+            if lamp.is_on() != True:
+                return False
+        return True
+
+    def check_off(self):
+        for lamp in self.lamp_list:
+            if lamp.is_on() != False:
+                return False
+        return True
--- test_models.py
+++ test_models.py
@@ -1,4 +1,5 @@
 import unittest
+from models import LampArray
 from models import ColorLamp
 
 class TestColorLamp(unittest.TestCase):
@@ -8,6 +9,25 @@ class TestColorLamp(unittest.TestCase):
     def test_set_color(self):
         self.lamp.set_color('Red')
         self.assertEqual(self.lamp.get_color(), 'Red')
+
+class TestLampArray(unittest.TestCase):
+    def setUp(self):
+        self.lamp = LampArray()
+    
+    def test_list_on(self):
+        self.lamp.add_lamp()
+        self.lamp.add_lamp()
+        self.lamp.set_on()
+        self.assertTrue(self.lamp.check_on())
+
+    def test_list_off(self):
+        self.lamp.add_lamp()
+        self.lamp.add_lamp()
+        self.lamp.set_off()
+        self.assertTrue(self.lamp.check_off())
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
